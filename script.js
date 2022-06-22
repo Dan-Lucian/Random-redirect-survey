ee2d1c90fdd41b20502483f0aeb59a2aaa66c00c
Dan-Lucian <danugaina1403@gmail.com>
2022-06-22 20:55:26 +0300

    change: removed completed surveys

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const webpages = [];
 
 // pink + features
-webpages.push("https://docs.google.com/forms/d/e/1FAIpQLSfDV2l77F8yMWIfx0DSV8jrUh3cdg2YgfNLV8nui_JvYP12Pg/viewform?usp=sf_link");
+// webpages.push("https://docs.google.com/forms/d/e/1FAIpQLSfDV2l77F8yMWIfx0DSV8jrUh3cdg2YgfNLV8nui_JvYP12Pg/viewform?usp=sf_link");
 
 // pink + no features
 webpages.push("https://docs.google.com/forms/d/e/1FAIpQLSceo5AMgrGK3XVA4_HokczyBVJ57jn3eoXDprQW5h2TcAkuHg/viewform?usp=sf_link");
@@ -10,7 +10,7 @@ webpages.push("https://docs.google.com/forms/d/e/1FAIpQLSceo5AMgrGK3XVA4_HokczyB
 webpages.push("https://docs.google.com/forms/d/e/1FAIpQLSf8Hr6rwbtuu6dMWKbul8V9uICoM1cYqLpF6UzoE8KXvFLQ9A/viewform?usp=sf_link");
 
 // blue + no features
-webpages.push("https://docs.google.com/forms/d/e/1FAIpQLSfPMJ3XnDip4sR8FPNH-JdPa1toJfsbyZJhmkJV6bAbRxP2pw/viewform?usp=sf_link");
+// webpages.push("https://docs.google.com/forms/d/e/1FAIpQLSfPMJ3XnDip4sR8FPNH-JdPa1toJfsbyZJhmkJV6bAbRxP2pw/viewform?usp=sf_link");
 
 
 const pageRandom = getRandomInt(0, webpages.length - 1);
